28c8d40e14bf8ce2c1338f076e54259d268212ff
IvanRusso91 <iv.russo91@gmail.com>
2022-07-14 15:19:09 +0200

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="box-x">
+
+        <h1>i miei post!</h1>
+
+        <div class="container-r">
+            <div class="box" v-for="post in posts" :key="post.id">
+                <h3>{{post.id}}. {{post.title}}</h3>
+                <p>{{post.content}}</p>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+    data(){
+        return{
+            apiUrl: 'http://127.0.0.1:8000/api/posts',
+            posts: [],
+        }
+    },
+
+    mounted(){
+        this.getApi();
+    },
+
+    methods:{
+        getApi(){
+            axios.get(this.apiUrl).then(r =>{
+                this.posts = r.data;
+            })
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
